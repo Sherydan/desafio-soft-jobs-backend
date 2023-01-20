58a9355fbc8fa2d014f78f0395c7abc8b688b1fd
Sherydan <tobar.luis.c@gmail.com>
2023-01-20 19:49:44 -0300

    server running

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+module.exports = app;
